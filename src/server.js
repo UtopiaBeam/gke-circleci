3ac52b0e791afd06f0ae84951eeea199e9d5af87
UtopiaBeam <utopiabeam@gmail.com>
2019-02-09 15:42:57 +0700

    Change API behaviour

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const lib = require('./lib');
 
 app.get('/ping', (req, res) => {
     const text = lib.ping();
-    return res.send(text);
+    return res.send({ messgae: text });
 });
 
 app.listen(3000, () => console.log('Listening to port 3000!'));
